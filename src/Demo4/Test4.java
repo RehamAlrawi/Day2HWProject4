b9a4932c8ae4e64c026a46a7353b0759b8e1651f
19495 <19495@ALRAWI.myfiosgateway.com>
2023-04-10 12:19:37 -0400

    multiply method

--- src/Demo4/Test4.java
+++ src/Demo4/Test4.java
@@ -4,5 +4,8 @@ public class Test4 {
 	public static void main(String[] args) {
 		System.out.println("Test4 In process...");
 	}
+	public void multiply( int a, int b) {
+		System.out.println(a * b );
+	}
 
 }
